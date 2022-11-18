f389ba5c40402443ba6990d50f24de1d8948a5ee
HyeonSeongKang <gt9697@naver.com>
2022-11-18 14:30:22 +0900

    init test

--- index.js
+++ index.js
@@ -13,13 +13,31 @@ try {
 } catch (err) {
   console.error(err);
 }
-var rtm = new RTMClient(token);
+
+var test_channel = "C04AE27LV0D";
+
+const rtm = new RTMClient(token);
 rtm.start();
 
+rtm.on("ready", async () => {
+  const rdy1 = await rtm.sendMessage("테스트를 시작한다.", test_channel);
+  console.log("테스트 루틴 시작이다.");
+  status++;
+
+  const rdy2 = await rtm.sendMessage("hi", test_channel);
+});
+
 rtm.on("message", function (message) {
-  var channel = message.channel;
   var text = message.text;
 
-  if (text == "hello") rtm.sendMessage("hi", channel);
-  else rtm.sendMessage("what?", channel);
+  console.log("받은 메시지: ", text);
+
+  if (status == 1) {
+    if (text == "Hello!") {
+      console.log("테스트#1 성공");
+    } else {
+      console.log("테스트#1 실패");
+    }
+  }
+  status++;
 });
